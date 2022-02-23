a102dc8cc2d74d41348b19377485ce6752634029
sararekowska <sara.rekowska@gmail.com>
2022-02-23 13:23:17 +0100

    add tailwind version

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "600px",
+      md: "770px",
+      lg: "990px",
+      xl: "1200px",
+    },
+    colors: {
+      lightgrey: "#7D7075",
+      grey: "#292C33",
+      white: "#FFFFFF",
+      orange: "#FE4A25",
+      shadow: "#3A3A3A",
+    },
+    fontFamily: {
+      body: ["TTNormsProRegular", "serif"],
+      light: ["TTNormsProLight", "serif"],
+    },
+  },
+};
